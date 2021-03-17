9936250286b30f52010bfdbb379f98c6e0191f3c
Hibrit <taskin.n.dogan@yandex.com>
2021-03-17 14:40:35 +0300

    added an vs code setup script

---
+++ vs_setup_script.sh
@@ -0,0 +1,20 @@
+#! before running this script please run minimal_setup_script and then open vs code atleast once
+
+#* installing default extensions of vs code
+
+code --install-extension jdinhlife.gruvbox
+code --install-extension oderwat.indent-rainbow
+code --install-extension ms-toolsai.jupyter
+code --install-extension pkief.material-icon-theme
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.python
+code --install-extension 2gua.rainbow-brackets
+code --install-extension aaron-bond.better-comments
+code --install-extension christian-kohler.path-intellisense
+
+#* download keybindings and default settings
+
+cd ~/.config/Code/User
+wget https://github.com/Hibrit/archlinuxsettings/raw/master/vscode/keybindings.json
+wget https://github.com/Hibrit/archlinuxsettings/raw/master/vscode/settings.json
